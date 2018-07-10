01d569c147dce3a9e34aa95bd79e461ea173bcc0
Kaitlyn Hova <kaitlynhova@gmail.com>
2018-07-09 22:46:23 -0700

    basic note data display

--- reactnotes/src/App.js
+++ reactnotes/src/App.js
@@ -1,18 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Note from "./Note/Note";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [{ id: 1, noteContent: "1" }, { id: 1, noteContent: "2" }]
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <h1 className="heading">React + Firebase To Do List</h1>
+        </div>
+        <div className="notesBody">
+          {this.state.notes.map(note => {
+            return (
+              <Note
+                noteContent={note.noteContent}
+                noteId={note.id}
+                key={note.id}
+              />
+            );
+          })}
+        </div>
+        <div className="notesFooter">
+          <p>Footer will go here</p>
+        </div>
       </div>
     );
   }
