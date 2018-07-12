41f5846d87420a3ec74e95b409ad60d66fa7c3b8
Kaitlyn Hova <kaitlynhova@gmail.com>
2018-07-11 21:39:56 -0700

    hooking up the app to firebase

--- reactnotes/src/App.js
+++ reactnotes/src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import Note from "./Note/Note";
 import NoteForm from "./NoteForm/NoteForm";
+import { DB_CONFIG } from "./Config/Config";
+import firebase from "firebase";
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.db = firebase
+      .database()
+      .ref()
+      .child("notes");
+
     this.state = {
-      notes: [{ id: 1, noteContent: "1" }, { id: 1, noteContent: "2" }]
+      notes: []
     };
+
     this.addNote = this.addNote.bind(this);
+    this.handleNewNote = this.handleNewNote.bind(this);
+
+    this.db.on("child_added", this.handleNewNote);
   }
+
+  handleNewNote(snap) {
+    this.setState({ notes: [...this.state.notes, snap.val().noteContent] });
+  }
+
   addNote(note) {
-    const previousNotes = this.state.notes;
-    previousNotes.push({
-      id: previousNotes.length + 1,
-      noteContent: note
-    });
-    this.setState({ notes: previousNotes });
+    debugger;
+    this.db.push({ noteContent: note });
   }
+
   render() {
     return (
       <div className="notesWrapper">
@@ -27,14 +41,8 @@ class App extends Component {
           <h1 className="heading">React + Firebase To Do List</h1>
         </div>
         <div className="notesBody">
-          {this.state.notes.map(note => {
-            return (
-              <Note
-                noteContent={note.noteContent}
-                noteId={note.id}
-                key={note.id}
-              />
-            );
+          {this.state.notes.map((note, i) => {
+            return <Note noteContent={note} noteId={i} key={i} />;
           })}
         </div>
         <div className="notesFooter">
@@ -46,3 +54,38 @@ class App extends Component {
 }
 
 export default App;
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.addNote = this.addNote.bind(this);
+//     this.app = new firebaseApp(DB_CONFIG); // firebaseApp.initializeApp(DB_CONFIG);
+
+//     // this.db = firebase
+//     //   .database()
+//     //   .ref()
+//     //   .child("notes");
+
+//     this.state = {
+//       notes: []
+//     };
+//   }
+//   componentWillMount() {
+//     const previousNotes = this.state.notes;
+//     this.db.on("child_added", snap => {
+//       console.log("ooo em gee", snap);
+//       // previousNotes.push({
+//       //   id: snap.key,
+//       //   noteContent: snap.val().noteContent
+//       // });
+//       // this.setState({
+//       //   notes: previousNotes
+//       // });
+//     });
+//   }
+//   addNote(noteContent) {
+//     debugger;
+//     this.db.push({ noteContent }); //.set({ noteContent: note });
+//   }
+// }
+
+// export default App;
